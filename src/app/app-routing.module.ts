e30a402057ac9e5075e30a049ccb2660c23aabd6
Alexys Santiago <SuxPorT@hotmail.com>
2023-06-13 20:00:38 -0300

    feat: #06 Route Transitions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,21 @@ import { TodosComponent } from './components/todos/todos.component';
 import { NotesComponent } from './components/notes/notes.component';
 
 const routes: Routes = [
-  { path: 'bookmarks', component: BookmarksComponent },
-  { path: 'todos', component: TodosComponent },
-  { path: 'notes', component: NotesComponent }
+  {
+    path: 'bookmarks',
+    component: BookmarksComponent,
+    data: { tab: 1 }
+  },
+  {
+    path: 'todos',
+    component: TodosComponent,
+    data: { tab: 2 }
+  },
+  {
+    path: 'notes',
+    component: NotesComponent,
+    data: { tab: 3 }
+  }
 ];
 
 @NgModule({
