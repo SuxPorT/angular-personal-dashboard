f156199d001ee6a11bbd11f30db18d8670a05bf5
Alexys Santiago <SuxPorT@hotmail.com>
2023-06-18 16:29:36 -0300

    feat: #13 Todo Service

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  getTodos(): Todo[] {
+    return this.todos;
+  }
+
+  getTodo(id: string): Todo | undefined {
+    return this.todos.find(t => t.id === id);
+  }
+
+  addTodo(todo: Todo): void {
+    this.todos.push(todo);
+  }
+
+  updateTodo(id: string, updateTodoFields: Partial<Todo>): void {
+    const todo = this.getTodo(id);
+
+    if (todo)
+      Object.assign(todo, updateTodoFields);
+  }
+
+  deleteTodo(id: string): void {
+    const index = this.todos.findIndex(t => t.id === id);
+    if (index == -1) return;
+
+    this.todos.splice(index, 1);
+  }
+
+}
