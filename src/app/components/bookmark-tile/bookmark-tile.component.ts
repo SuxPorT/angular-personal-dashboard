62c82f110a36ac97f1d4815a54ea2e3fa9df7e12
Alexys Santiago <SuxPorT@hotmail.com>
2023-06-23 20:42:50 -0300

    feat: #20 Generating the Bookmark Tiles

--- src/app/components/bookmark-tile/bookmark-tile.component.ts
+++ src/app/components/bookmark-tile/bookmark-tile.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Bookmark } from 'src/app/models/bookmark.model';
 
 @Component({
   selector: 'app-bookmark-tile',
   templateUrl: './bookmark-tile.component.html',
   styleUrls: ['./bookmark-tile.component.scss']
 })
-export class BookmarkTileComponent {
+export class BookmarkTileComponent implements OnInit {
+
+  @Input() bookmark!: Bookmark;
+
+  titleIconSrc: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.titleIconSrc = `${this.bookmark.url.origin}/favicon.ico`;
+  }
 
 }
--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Bookmark } from 'src/app/models/bookmark.model';
+import { BookmarkService } from 'src/app/services/bookmark.service';
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
   styleUrls: ['./bookmarks.component.scss']
 })
-export class BookmarksComponent {
+export class BookmarksComponent implements OnInit {
+
+  bookmarks: Bookmark[] = [];
+
+  constructor(private bookmarkService: BookmarkService) { }
+
+  ngOnInit(): void {
+    this.bookmarks = this.bookmarkService.getBookmarks();
+  }
 
 }
